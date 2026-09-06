66ab2071560ee33403ab6467b3b6aeb22f1542e5
Jayanta Garai <jayanta.20244085@mnnit.ac.in>
2026-09-06 23:04:33 +0530

    Production level Setup done

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,39 @@
+import { v2 as cloudinary } from 'cloudinary';
+import fs from "fs"
+// configuration
+
+cloudinary.config({
+    cloud_name:process.env.CLOUDINARY_CLOUD_NAME, 
+    api_key:process.env.CLOUDINARY_API_KEY, 
+    api_secret:process.env.CLOUDINARY_API_SECRET
+});
+
+const uploadOnCloudinary=async (localPath)=>{
+    try{
+        if(!localPath){
+            return null;
+        }
+        const response=await cloudinary.uploader.upload(localPath,{
+            resource_type:"auto"
+        })
+        console.log("File is uploaded successfully.",response.url);
+        return response;
+    }catch(error){
+        fs.unlinkSync(localPath); // remove the locally saved file as the upload is failed
+        console.log(error);
+        return null;
+    }
+}
+
+// 
+const uploadResult = await cloudinary.uploader
+       .upload(
+           'https://res.cloudinary.com/demo/image/upload/getting-started/shoes.jpg', {
+               public_id: 'shoes',
+           }
+       )
+       .catch((error) => {
+           console.log(error);
+       });
+    
+    console.log(uploadResult);
